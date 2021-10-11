84e22d26f4dac51891e66ef42a9541e98ff4041c
Jay-R-H <jayhiremathr@gmail.com>
2021-10-11 21:45:23 +0530

    Added:Method to insert number and display BST

--- src/com/programming/BinarySearchTreeRunner.java
+++ src/com/programming/BinarySearchTreeRunner.java
@@ -4,5 +4,13 @@ public class BinarySearchTreeRunner {
 
     public static void main(String[] args) {
 
+        BinarySearchOperation binary = new BinarySearchOperation();
+        // Inserting data.
+        binary.insert(56);
+        binary.insert(30);
+        binary.insert(70);
+        // Displaying the Binary tree.
+        binary.inorderTraversal(binary.root);
+
     }
 }
--- src/com/programming/Node.java
+++ src/com/programming/Node.java
@@ -1,5 +1,13 @@
 package com.programming;
 
 public class Node {
+    int data;
+    Node left;
+    Node right;
 
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
